67cec016c41a6029510c3aa567e1bf24e252a12b
SuddenlyBanana <82901591+SuddenlyBanana@users.noreply.github.com>
2025-06-11 21:49:53 +0200

    add stateVersion

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }:
 
 {
+  system.stateVersion = "25.05";
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   environment.systemPackages = with pkgs; [ wget curl ];
